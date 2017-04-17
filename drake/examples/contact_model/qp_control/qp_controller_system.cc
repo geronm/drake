af5a8cfacc555ef5338c0ef59ffbc07e56c69859
Geronimo J Mirano <geronm@mit.edu>
2017-04-17 03:51:42 -0400

    Added QP System.
    Also, discovered an issue, can't call StepTo if there's
    a DoCalcUnrestrictedUpdate that is firing synchronously
    with the current timestep --> leads to infinite loop.

---
+++ drake/examples/contact_model/qp_control/qp_controller_system.cc
@@ -0,0 +1,306 @@
+#include "drake/examples/contact_model/qp_control/qp_controller_system.h"
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "drake/lcmt_inverse_dynamics_debug_info.hpp"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace examples {
+
+namespace {
+
+template <typename ValueType>
+ValueType& get_mutable_value(systems::State<double>* state, int index) {
+  DRAKE_DEMAND(state);
+  return state->get_mutable_abstract_state()
+      ->get_mutable_value(index)
+      .GetMutableValue<ValueType>();
+}
+
+}  // namespace
+
+
+  // int input_port_index_q_manip_desired_{0};
+  // int output_port_index_x_desired_u_{0};
+QpControllerSystem::QpControllerSystem(const RigidBodyTree<double>& robot,
+                                       double dt, double alpha, const std::vector<int> control_input_indices)
+    : robot_(robot), control_dt_(dt), control_alpha_(alpha),
+      qp_controller_(1.0, 1.0),
+      finger_forwardkin_xyz_(3, 4),
+      control_input_indices_(control_input_indices) {
+  input_port_index_q_manip_desired_ = DeclareInputPort(systems::kVectorValued, 2).get_index();
+  output_port_index_x_desired_u_ = DeclareOutputPort(systems::kVectorValued, control_input_indices_.size()).get_index();
+  // DeclareContinuousState(kXDesiredULength + kQManipLength);
+
+  // Store useful data for retrieving the proper finger points
+  finger_forwardkin_names_.push_back("finger1_paddle");
+  finger_forwardkin_names_.push_back("finger1_paddle");
+  finger_forwardkin_names_.push_back("finger2_paddle");
+  finger_forwardkin_names_.push_back("finger2_paddle");
+  for (size_t r=0; r<3; r++) {
+    for (size_t c=0; c<4; c++) {
+      finger_forwardkin_xyz_(r,c) = 0.0;
+      if (r==0) {
+        if (c % 2 == 0) {
+          finger_forwardkin_xyz_(r,c) = kPaddleHalfWidth;
+        } else {
+          finger_forwardkin_xyz_(r,c) = -kPaddleHalfWidth;
+        }
+      }
+    }
+  }
+  std::cout << "Hello from QP Controller System!:" << std::endl;
+  std::cout << finger_forwardkin_xyz_ << std::endl;
+
+  set_name("QpControllerSystem");
+  DeclarePeriodicUnrestrictedUpdate(control_dt_, 0);
+}
+
+void QpControllerSystem::DoCalcOutput(
+    const systems::Context<double>& context,
+    systems::SystemOutput<double>* output) const {
+
+
+  if (context.get_time() >= 0.18) {
+    // std::cout << "DoCalcOutput" << std::endl;
+  }
+  
+  // Our only output is x_desired_u, which is stored in the state itself
+
+  // drake::systems::BasicVector<double>* qp_output = output->GetMutableVectorData(output_port_index_x_desired_u_);
+  // for (size_t i=0; i < (size_t)qp_output->size(); i++) {
+  //   qp_output->SetAtIndex(i,0.0);
+  // }
+  // for (size_t u=0; u<control_input_indices_.size()/2; u++) {
+  //   qp_output->SetAtIndex(control_input_indices_[u], context.get_discrete_state(0)->GetAtIndex(u));
+  // }
+
+  // Obtain the structure we need to write into.
+  systems::BasicVector<double>* const output_vector =
+      output->GetMutableVectorData(output_port_index_x_desired_u_);
+  DRAKE_ASSERT(output_vector != nullptr);
+
+  // Output port value is just the continuous state.
+  // for (size_t i=0; i<12; i++) {
+  //   (output_vector->get_mutable_value())(i) = 0.0;
+  // }
+
+  if (context.get_time() >= 0.18) {
+    // std::cout << context.get_time() << std::endl;
+    // std::cout << context.get_discrete_state(0)->CopyToVector().transpose() << std::endl;
+    // std::cout << output_vector->get_mutable_value().transpose() << std::endl;
+  }
+
+  for (size_t u=0; u<(size_t)control_input_indices_.size(); u++) {
+    (output_vector->get_mutable_value())(u) =
+          (context.get_discrete_state(0)->CopyToVector())(u);
+  }
+}
+
+
+void QpControllerSystem::DoCalcUnrestrictedUpdate(
+    const systems::Context<double>& context,
+    systems::State<double>* state) const {
+  std::cout << "Doing DoCalcUnrestrictedUpdate..." << std::endl;
+
+  // Inputs:
+  // const Eigen::VectorXd* q_manip_desired = EvalInputValue<Eigen::VectorXd>(
+  //     context, input_port_index_q_manip_desired_);
+
+  // // const Eigen::VectorXd*
+  // const BasicVector<double>* q_manip_desired_bv = EvalVectorInput(context, input_port_index_q_manip_desired_);
+  // // const Eigen::VectorXd q_manip_desired(q_manip_desired_bv->size());
+  // const Eigen::VectorXd q_manip_desired = q_manip_desired_bv->CopyToVector();
+
+
+  const Eigen::VectorXd q_manip_desired = EvalVectorInput(context, input_port_index_q_manip_desired_)->CopyToVector();
+  
+
+  // Gets the x state
+  // Eigen::VectorXd& x_desired_u =
+  //     get_mutable_value<Eigen::VectorXd>(state, kAbstractStateIndexXDesiredU);
+
+  // // // // systems::ContinuousState<double>* mutable_state = state->get_mutable_continuous_state();
+  // systems::DiscreteState<double>* mutable_state = state->get_mutable_discrete_state(0);
+  //drake::systems::VectorBase<double>* state_vector = mutable_state->get_mutable_discrete_state();
+  // Eigen::VectorXd state_vector_eigen(kXDesiredULength + kQManipLength);
+  // for (size_t i=0; i<kXDesiredULength + kQManipLength; i++) {
+  //   state_vector_eigen(i) = mutable_state-> // state_vector->GetAtIndex(i);
+  // }
+
+  std::cout << "Print ABC" << std::endl;
+  auto x = context.get_discrete_state(0)->get_value();
+  Eigen::VectorXd state_vector_eigen(kXDesiredULength + kQManipLength);
+  for (size_t i=0; i<kXDesiredULength + kQManipLength; i++) {
+    state_vector_eigen(i) = x[i]; // state_vector->GetAtIndex(i);
+  }
+
+  std::cout << "Print D" << std::endl;
+  // Finds the four finger points from x_desired_u
+  Eigen::VectorXd p_ij(8);
+  p_ij.setZero();
+
+  Eigen::VectorXd q_fake(this->robot_.get_num_positions());
+  q_fake.setZero();
+  for (size_t u=0; u<control_input_indices_.size()/2; u++) {
+    q_fake(control_input_indices_[u]) = state_vector_eigen(u); // x_desired_u(u);
+  }
+  KinematicsCache<double> cache = this->robot_.doKinematics(q_fake);
+
+  std::cout << "q_fake: " << q_fake.transpose() << std::endl;
+
+  std::cout << "Print E" << std::endl;
+  // Get the four points
+  for (size_t f=0; f<finger_forwardkin_names_.size(); f++) {
+    Eigen::Vector3d xyz_vec;
+    xyz_vec(0) = finger_forwardkin_xyz_(0,f);
+    xyz_vec(1) = finger_forwardkin_xyz_(1,f);
+    xyz_vec(2) = finger_forwardkin_xyz_(2,f);
+    Eigen::Vector3d p_finger = this->robot_.template transformPoints<double>(cache, xyz_vec,
+                                        this->robot_.FindBodyIndex(finger_forwardkin_names_[f]),
+                                        0);
+    p_ij(2*f + 0) = p_finger(0);
+    p_ij(2*f + 1) = p_finger(1);
+    // Don't care about z-value of p_finger
+  }
+
+  // Reduce to point-case by collapsing points
+
+  std::cout << "Print F" << std::endl;
+  Eigen::VectorXd q_manip_ss(2);
+  Eigen::MatrixXd d_pmanip_d_pij(2,8);
+  qp_controller_.getQManipAndJac(p_ij, q_manip_ss, d_pmanip_d_pij);
+
+  // Go from pointwise-Jacobian back to statewise-Jacobian
+  Eigen::MatrixXd d_pij_d_q(8,q_fake.size());
+  for (size_t f=0; f<finger_forwardkin_names_.size(); f++) {
+    Eigen::Vector3d xyz_vec;
+    xyz_vec(0) = finger_forwardkin_xyz_(f,0);
+    xyz_vec(1) = finger_forwardkin_xyz_(f,1);
+    xyz_vec(2) = finger_forwardkin_xyz_(f,2);
+    Eigen::MatrixXd d_p_d_q = this->robot_.template transformPointsJacobian<double>(cache, xyz_vec,
+                                        this->robot_.FindBodyIndex(finger_forwardkin_names_[f]),
+                                        0,
+                                        true);
+    for (size_t i=0; i<(size_t)q_fake.size(); i++) {
+      d_pij_d_q(2*f+0, i) = d_p_d_q(0, i);
+      d_pij_d_q(2*f+1, i) = d_p_d_q(1, i);
+    }
+  }
+  Eigen::MatrixXd d_pmanip_d_q = d_pmanip_d_pij * d_pij_d_q;
+
+  std::cout << "p_ij:" << std::endl;
+  std::cout << p_ij.transpose() << std::endl;
+  std::cout << "d_pij_d_q: " << std::endl;
+  std::cout << d_pij_d_q << std::endl;
+  std::cout << "d_pmanip_d_pij: " << std::endl;
+  std::cout << d_pmanip_d_pij << std::endl;
+  std::cout << "d_pmanip_d_q: " << std::endl;
+  std::cout << d_pmanip_d_q << std::endl;
+
+  std::cout << "Print G" << std::endl;
+  // Update x_desired_u via the Jacobian control update rule
+  Eigen::MatrixXd q_manip_error = q_manip_desired - q_manip_ss;
+  Eigen::VectorXd delta_x_desired = control_alpha_*control_dt_*(d_pmanip_d_q.transpose())*q_manip_error;
+
+  std::cout << "delta_x_desired: " << std::endl;
+  std::cout << delta_x_desired.transpose() << std::endl;
+
+  for (size_t u=0; u<control_input_indices_.size(); u++) {
+    if (u < control_input_indices_.size()/2) {
+      // x_desired_u(u) += delta_x_desired[(control_input_indices_[u])]; 
+      state_vector_eigen(u) += delta_x_desired[(control_input_indices_[u])]; 
+    } else {
+      // x_desired_u(u) = 0; // qdot desired is always 0
+      state_vector_eigen(u) = 0; // qdot desired is always 0
+    }
+  }
+
+  // Final guard: project x_desired_u back into safety 
+  // TODO Make proper guard; for now, it just says don't go above or below a certain amount
+  double safety_limit = 3.0;
+  for (size_t i=0; i < (size_t)state_vector_eigen.size(); i++) {
+    if (state_vector_eigen(i) > safety_limit) {
+      state_vector_eigen(i) = safety_limit;
+    } else if (state_vector_eigen(i) < -safety_limit) {
+      state_vector_eigen(i) = -safety_limit;
+    }
+  }
+  if (state_vector_eigen(0) - state_vector_eigen(3) > -.1) {
+    std::cout << "Over!!" << std::endl;
+    double avg = .5 * (state_vector_eigen(0) + state_vector_eigen(3));
+    state_vector_eigen(0) = avg - .05;
+    state_vector_eigen(3) = avg + .05;
+  } else if (state_vector_eigen(0) - state_vector_eigen(3) < -.2) {
+    std::cout << "Over!!" << std::endl;
+    double avg = .5 * (state_vector_eigen(0) + state_vector_eigen(3));
+    state_vector_eigen(0) = avg - .1;
+    state_vector_eigen(3) = avg + .1;
+  }
+  if (state_vector_eigen(2) - state_vector_eigen(5) > 1.0) {
+    std::cout << "Over2!!" << std::endl;
+    double avg = .5 * (state_vector_eigen(2) + state_vector_eigen(5));
+    state_vector_eigen(2) = avg + .5;
+    state_vector_eigen(5) = avg - .5;
+  } else if (state_vector_eigen(2) - state_vector_eigen(5) < -1.0) {
+    std::cout << "Over2!!" << std::endl;
+    double avg = .5 * (state_vector_eigen(2) + state_vector_eigen(5));
+    state_vector_eigen(2) = avg - .5;
+    state_vector_eigen(5) = avg + .5;
+  }
+
+  std::cout << "Print H" << std::endl;
+  // Put state_vector_eigen back into state vector
+  state->get_mutable_discrete_state()->get_mutable_discrete_state(0)->SetFromVector(state_vector_eigen);
+
+  // Some prints
+  std::cout << "current q_manip_desired: " << q_manip_desired.transpose() << std::endl;
+  std::cout << "current q_manip_ss: " << q_manip_ss.transpose() << std::endl;
+  std::cout << "current state_vector_eigen: " << state_vector_eigen.transpose() << std::endl;
+}
+
+// std::unique_ptr<systems::AbstractValues>
+// QpControllerSystem::AllocateAbstractState() const {
+//   std::vector<std::unique_ptr<systems::AbstractValue>> abstract_vals(2);
+//   abstract_vals[kAbstractStateIndexXDesiredU] =
+//       std::unique_ptr<systems::AbstractValue>(
+//           new systems::Value<Eigen::VectorXd>(Eigen::VectorXd(kXDesiredULength)));
+//   abstract_vals[kAbstractStateIndexCachedP] =
+//       std::unique_ptr<systems::AbstractValue>(
+//           new systems::Value<Eigen::VectorXd>(Eigen::VectorXd(kQManipLength)));
+//   return std::make_unique<systems::AbstractValues>(std::move(abstract_vals));
+// }
+
+// // // // std::unique_ptr<ContinuousState<double>> QpControllerSystem::AllocateContinuousState()
+// // // //     const {
+// // // //   return make_unique<ContinuousState<double>>(
+// // // //       make_unique<BasicVector<double>>(get_num_states()),
+// // // //       get_num_positions() /* num_q */, get_num_velocities() /* num_v */,
+// // // //       0 /* num_z */);
+// // // // }
+
+std::unique_ptr<systems::DiscreteState<double>> QpControllerSystem::AllocateDiscreteState()
+    const {
+  std::cout << "Doing AllocateDiscreteState..." << std::endl;
+
+  return std::make_unique<systems::DiscreteState<double>>(
+      std::make_unique<systems::BasicVector<double>>(kXDesiredULength + kQManipLength));
+}
+
+// std::unique_ptr<systems::AbstractValue>
+// QpControllerSystem::AllocateOutputAbstract(
+//     const systems::OutputPortDescriptor<double>& descriptor) const {
+//   if (descriptor.get_index() == output_port_index_x_desired_u_) {
+//     return systems::AbstractValue::Make<Eigen::VectorXd>(
+//         Eigen::VectorXd(kXDesiredULength));
+//   } else {
+//     DRAKE_DEMAND(false);
+//     return nullptr;
+//   }
+// }
+
+}  // namespace examples
+}  // namespace drake
---
+++ drake/examples/contact_model/qp_control/qp_controller_system.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <memory>
+
+#include "drake/common/drake_copyable.h"
+#include "drake/examples/contact_model/qp_control/qp_controller.h"
+#include "drake/multibody/rigid_body_tree.h"
+#include "drake/systems/framework/leaf_system.h"
+
+namespace drake {
+namespace examples {
+
+/**
+ * A discrete time system block for an inverse dynamics controller.
+ */
+class QpControllerSystem : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(QpControllerSystem)
+
+  /**
+   * Constructor for the inverse dynamics controller.
+   * @param robot Reference to a RigidBodyTree. Its lifespan must be longer
+   * than this object.
+   * @param dt Control cycle period.
+   * @param alpha Parameter for Jacobian controller. On each timestep,
+   *    change x by -alpha*dt*Jinv*error
+   */
+  QpControllerSystem(const RigidBodyTree<double>& robot, double dt, double alpha, const std::vector<int> control_input_indices);
+
+  void DoCalcOutput(const systems::Context<double>& context,
+                    systems::SystemOutput<double>* output) const override;
+
+  // std::unique_ptr<systems::AbstractValue> AllocateOutputAbstract(
+  //     const systems::OutputPortDescriptor<double>& descriptor) const override;
+
+  // std::unique_ptr<systems::AbstractValues> AllocateAbstractState()
+  //     const override;
+
+  // // // // std::unique_ptr<ContinuousState<double>> AllocateContinuousState() const override;
+
+  std::unique_ptr<systems::DiscreteState<double>> AllocateDiscreteState() const override;
+
+  void DoCalcUnrestrictedUpdate(const systems::Context<double>& context,
+                                systems::State<double>* state) const override;
+
+  /**
+   * Returns the input port for HumanoidStatus.
+   */
+  inline const systems::InputPortDescriptor<double>&
+  get_input_port_q_manip_desired() const {
+    return get_input_port(input_port_index_q_manip_desired_);
+  }
+
+  // /**
+  //  * Returns the input port for QpInput.
+  //  */
+  // inline const systems::InputPortDescriptor<double>& get_input_port_qp_input()
+  //     const {
+  //   return get_input_port(input_port_index_qp_input_);
+  // }
+
+  /**
+   * Returns the output port for QpOutput.
+   */
+  inline const systems::OutputPortDescriptor<double>&
+  get_output_port_x_desired_u() const {
+    return get_output_port(output_port_index_x_desired_u_);
+  }
+
+  // /**
+  //  * Returns the output port for lcmt_inverse_dynamics_debug_info.
+  //  */
+  // inline const systems::OutputPortDescriptor<double>&
+  // get_output_port_debug_info() const {
+  //   return get_output_port(output_port_index_debug_info_);
+  // }
+
+  inline double get_control_dt() const { return control_dt_; }
+
+ private:
+  const RigidBodyTree<double>& robot_;
+  const double control_dt_{};
+  const double control_alpha_{};
+  // Indices into AbstractState. These are updated by DoCalcUnrestrictedUpdate,
+  // and DoCalcOutput copies them to the output ports.
+  const int kAbstractStateIndexXDesiredU{0};
+  const int kAbstractStateIndexCachedP{1};
+
+  const double kPaddleHalfWidth{4.0};
+  const double kBoxWidth{2.0};
+
+  const size_t kXDesiredULength{12};
+  const size_t kQManipLength{2};
+
+  // TODO(siyuan.feng): This is a bad temporary hack to the const constraint for
+  // CalcOutput. It is because qp controller needs to allocate mutable workspace
+  // (MathematicalProgram, temporary matrices for doing math, etc),
+  // and I want to avoid allocating these repeatedly.
+  // This should be taken care of with the new system2 cache.
+  mutable QPController qp_controller_;
+
+  Eigen::Matrix<double,3,4> finger_forwardkin_xyz_;
+  std::vector<std::string> finger_forwardkin_names_;
+  const std::vector<int> control_input_indices_;
+
+  // int input_port_index_humanoid_status_{0};
+  // int input_port_index_qp_input_{0};
+  int input_port_index_q_manip_desired_{0};
+  // int output_port_index_qp_output_{0};
+  int output_port_index_x_desired_u_{0};
+};
+
+}  // namespace examples
+}  // namespace drake
